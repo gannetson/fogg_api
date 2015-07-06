5d9479ed5a27a56871c6d912134fa5c97ed066d5
Loek van Gent <loek@1procentclub.nl>
2015-07-06 10:23:10 +0200

    Change serializer

--- apps/countries/models.py
+++ apps/countries/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Country(models.Model):
 
     name = models.CharField(max_length=200)
-    code2 = models.CharField(max_length=2, blank=True, help_text="ISO 3166-1 alpha-2 code")
+    code2 = models.CharField(max_length=2, blank=True, help_text="ISO 3166-1 alpha-2 code", unique=True)
     code3 = models.CharField(max_length=4, blank=True, help_text="ISO 3166-1 alpha-3 code")
     kml = models.CharField(max_length=500000, blank=True, default="")
     geojson = models.CharField(max_length=500000, blank=True, default="")
--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from apps.users.models import User
+from apps.countries.models import Country
 
 
 class UserSerializer(serializers.ModelSerializer):
 
-    countries = serializers.SlugRelatedField(slug_field='code2', queryset='countries', many=True)
+    countries = serializers.SlugRelatedField(slug_field='code2', queryset=Country.objects, many=True)
 
     class Meta:
         model = User
